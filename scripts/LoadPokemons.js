8fbebdc289c64e5e1469c028966ae3085f7d80cc
jg_stadler <joaogbstadler@gmail.com>
2026-01-28 14:45:37 -0300

    Sistema de busca implementado

--- scripts/LoadPokemons.js
+++ scripts/LoadPokemons.js
@@ -1,7 +1,9 @@
 const poke_cards_container = document.querySelector(".poke-container");
-const urlBase = "https://pokeapi.co/api/v2/pokemon/";
+const urlBase = "https://pokeapi.co/api/v2/pokemon/";                   //Ativação do carregamento dos pokemons ao carregar a pagina
 window.addEventListener('load',()=>{fetchApiInfo(urlBase)});
 
+
+// Consulta da api
 const fetchApiInfo = async(url) => {
     try{
         const response = await fetch(url);
@@ -13,14 +15,16 @@ const fetchApiInfo = async(url) => {
         LoadPokemons(pokemons_list.results);
     }
     catch{
-        console.log("Nada encontrado")
+        poke_cards_container.innerHTML = '<div class="pokemons-not-found mt-5">Nada encontrado</div>';
     }
 }
+
+// Função de carregamentos dos cartões com os pokemons
+
 LoadPokemons = (pokemons_array) => {
     poke_cards_container.innerHTML = ""; //Limpando o container para inserir o novo conteúdo
 
     pokemons_array.map((pokemon, i) => {
-        console.log(pokemon)
         poke_cards_container.innerHTML += 
         `
             <div class="pokecard d-grid mt-3 rounded">
@@ -36,3 +40,35 @@ LoadPokemons = (pokemons_array) => {
         `;
     });
 }
+
+// Mecanismo de busca
+
+const searchForm = document.getElementById("search-form");
+
+searchForm.addEventListener('submit',async(e)=>{
+    e.preventDefault();
+    poke_cards_container.innerHTML = '<div class="spinner-border mt-5" role="status"></div>'; //Adicionando a animação de carregamento
+    const pokemonName = document.getElementById("input-pokename").value;
+    const urlToSearch = `https://pokeapi.co/api/v2/pokemon/${pokemonName}` //Url com o nome do pokemon desejado pelo usuário
+    try{
+        const response = await fetch(urlToSearch); //Consulta da api com a url personalizada pelo usuário
+        const pokemonFromSearch = await response.json();
+        poke_cards_container.innerHTML = ''; //Limpando conteúda para adição do resultado da pesquisa
+        poke_cards_container.innerHTML += 
+            `
+                <div class="pokecard d-grid mt-3 rounded">
+                    <div class="pokeball-logo"><img src="assets/img/favicon.ico"></div>
+                    <h1 class="pokemon-name">${pokemonFromSearch.name}</h1>
+                    <div class="pokeclas">🔥</div>
+                    <picture class="forest-bg rounded">
+                        <img src="${pokemonFromSearch.sprites.front_default}"
+                        alt="Imagem do pokemon" class="pokemon-img">
+                    </picture>
+                    <button class="poke-info-btn btn btn-light p-1" >Mais informações</button>
+                </div>
+        `;
+        }
+        catch{
+            poke_cards_container.innerHTML = '<div class="pokemons-not-found mt-5">Nada encontrado</div>';
+        }
+})
